04dae941a50ff4c7e50de3c66e082b24b61f6011
Ayumi Saito <ayumisaito27@gmail.com>
2017-11-08 10:39:58 +0100

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-
-
+const path = require('path')
 const app = express();
 
 // Use static file from the React app
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', (req,res) => {
 	res.json("hello")
 })
+
+// if request does not match, return React's index.html file
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
